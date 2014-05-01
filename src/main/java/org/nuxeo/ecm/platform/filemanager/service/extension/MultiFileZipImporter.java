49db7443db23c4edf9efaf6a4fba8052048b48ee
Mike Obrebski <mobrebski@nuxeo.com>
2014-05-01 16:41:43 -0400

    Added duplicate check to Folders

--- src/main/java/org/nuxeo/ecm/platform/filemanager/service/extension/MultiFileZipImporter.java
+++ src/main/java/org/nuxeo/ecm/platform/filemanager/service/extension/MultiFileZipImporter.java
@@ -6,7 +6,6 @@ package org.nuxeo.ecm.platform.filemanager.service.extension;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Serializable;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Enumeration;
@@ -22,6 +21,7 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.PathRef;
 import org.nuxeo.ecm.core.api.impl.blob.InputStreamBlob;
+import org.nuxeo.ecm.platform.filemanager.utils.FileManagerUtils;
 import org.nuxeo.ecm.platform.types.TypeManager;
 
 /**
@@ -55,60 +55,58 @@ public class MultiFileZipImporter extends AbstractFileImporter {
             String path, boolean overwrite, String filename,
             TypeManager typeService) throws ClientException, IOException {
 
+        // Get and check Zip file
         File tmp = File.createTempFile("multifilezip-importer", null);
-
         content.transferTo(tmp);
-
         ZipFile zip = getArchiveFileIfValid(tmp);
-
         if (zip == null) {
             tmp.delete();
             return null;
         }
+
         DocumentModel container = documentManager.getDocument(new PathRef(path));
 
-
-        //Process each folder
+        // Process each folder
         Enumeration<? extends ZipEntry> entries = zip.entries();
         while (entries.hasMoreElements()) {
-            //Process each directory
             ZipEntry entry = entries.nextElement();
-            //Get path and names
+            // Get path and names
             String name = entry.getName();
 
-            //Parse path
+            // Parse path
             Path filePath = Paths.get(name);
             Path rootPath = filePath.getParent();
             String rootName = "";
             if (rootPath != null) {
-                rootName = "/"+rootPath.toString();
+                rootName = "/" + rootPath.toString();
             }
             String fileName = filePath.getFileName().toString();
-            log.debug(path+rootName+"/"+fileName);
 
-            if ( entry.isDirectory() ){
-                DocumentModel folder = documentManager.createDocumentModel(path+rootName, fileName, "Folder");
-                folder.setPropertyValue("dc:title", fileName);
-                documentManager.createDocument(folder);
+            if (entry.isDirectory()) {
+                log.info(path + rootName + " / " + fileName);
+                // Check if exists
+                DocumentModel folder = FileManagerUtils.getExistingDocByTitle(
+                        documentManager, path + rootName, fileName);
+
+                // Create Folder if it doesn't exist already by name
+                if (folder == null) {
+                    log.info("Creating: " + path + rootName + " / " + fileName);
+                    folder = documentManager.createDocumentModel(path
+                            + rootName, fileName, "Folder");
+                    folder.setPropertyValue("dc:title", fileName);
+                    documentManager.createDocument(folder);
+                }
             }
 
             else {
 
-                // create document model
-                DocumentModel file = documentManager.createDocumentModel(path+rootName, fileName, "File");
-                file.setPropertyValue("dc:title", fileName);
-                file.setPropertyValue("file:filename", fileName);
-
+                // Create Files (Automatically versions duplicates by name)
                 InputStream blobStream = zip.getInputStream(entry);
                 Blob blob = new InputStreamBlob(blobStream);
                 blob.setFilename(fileName);
 
-                // update data
-                file.setPropertyValue("file:content", (Serializable)blob);
-                // create
-
-                documentManager.createDocument(file);
-
+                fileManagerService.createDocumentFromBlob(documentManager,
+                        blob, path + rootName, overwrite, fileName);
 
             }
         }
